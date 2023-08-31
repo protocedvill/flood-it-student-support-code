9c4c15f401f742f1d06f54ea3ffe7b3adfd86779
Tianyu Chen <chentianyu@outlook.com>
2023-08-31 14:20:12 -0400

    import

---
+++ src/Flood.java
@@ -0,0 +1,25 @@
+// Imports for the parameters of flood
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+
+public class Flood {
+
+    // Students implement this flood function.
+    public static void flood(WaterColor color,
+                              LinkedList<Coord> flooded_list,
+                              Tile[][] tiles,
+                              Integer board_size) {
+        // YOUR CODE
+    }
+
+    // An alternative implementation goes here.
+    public static void flood1(WaterColor color,
+                             LinkedList<Coord> flooded_list,
+                             Tile[][] tiles,
+                             Integer board_size) {
+        // YOUR CODE
+    }
+
+}
